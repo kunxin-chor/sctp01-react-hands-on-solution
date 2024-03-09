4f09039b41f3dd1ce857745aec0eef345753c3c1
kunxin-chor <chorkunxin@yahoo.com>
2024-03-09 08:47:22 +0000

    Added solutions

--- 05-basic-forms/05-basic-forms/src/AreaForm.js
+++ 05-basic-forms/05-basic-forms/src/AreaForm.js
@@ -1,28 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export default function AreaForm(){
-  
-    return (
-      <React.Fragment>
-        <div>
-          <label>Width:</label>
-          <input type="text" />
-        </div>
-        <div>
-          <label>Height:</label>
-          <input type="text" />
-        </div>
-        <div>Area = </div>
-      </React.Fragment>
-    )
-  
-    const updateWidth =  (event) => {
-        
-    }
+export default function AreaForm() {
 
-    const updateHeight = (event) => {
-         
-    }
+  const [width, setWidth] = useState(0);
+  const [height, setHeight] = useState(0);
+
+  const updateWidth = (event) => {
+    setWidth(event.target.value);
+  }
+
+  const updateHeight = (event) => {
+    setHeight(event.target.value)
+  }
+
+  let area = 0;
+  if (width > 0 && height > 0) {
+    area = width * height;
+  }
+
+  return (
+    <React.Fragment>
+      <div>
+        <label>Width:</label>
+        <input type="text" value={width} onChange={updateWidth} />
+      </div>
+      <div>
+        <label>Height:</label>
+        <input type="text" value={height} onChange={updateHeight} />
+      </div>
+      <div>Area = {area}</div>
+    </React.Fragment>
+  )
+
+
 
 }
 
